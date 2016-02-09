2df68a30dde7aefd4a82731ca6ac543817ddefb1
aneeshvaidya <aneesh.h.vaidya@gmail.com>
2016-02-09 02:24:04 +0000

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,15 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word 
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @game_state = :play
   end
 
   def self.get_random_word
@@ -18,5 +25,58 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def guess(letter)
+    
+    if letter.nil? or /[^a-zA-Z]/.match(letter) or letter.empty?
+      raise ArgumentError.new("Invalid argument.")
+    end
+    letter = letter.downcase
+    if @word.include? letter
+      if !@guesses.include? letter
+        @guesses += letter
+      else
+        false
+      end
+    else
+      if !@wrong_guesses.include? letter
+        @wrong_guesses += letter
+      else
+        false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    word = ''
+    @word.each_char do |letter|
+      if @guesses.include? letter
+        word += letter
+      else
+        word += '-'
+      end
+    end
+    word
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      @game_state = :lose
+    else
+      word = ''
+      @word.each_char do |letter|
+        if @guesses.include? letter
+          word += letter
+        end
+      end
+      if @word == word
+        @game_state = :win
+      else
+        @game_state = :play
+      end
+    end
+    @game_state
+  end
+  
+  
 end
